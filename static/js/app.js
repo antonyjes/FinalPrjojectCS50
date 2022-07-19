9878247ed361f9a0091c38ae87cbbdc6e191a718
antonyjes <72809149@continental.edu.pe>
2022-07-19 17:06:11 -0500

    Compare two passwords in live

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    // Compare two passwords and validate
     $('#password2').keyup(function() {
         if ($('#password1').val() != $('#password2').val()) {
             $('#msg-passwords').html("Password do not match").css('background-color', 'red');
