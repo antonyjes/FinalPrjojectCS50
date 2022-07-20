aefa41d20a726af0711748c97140da6b10b508dd
antonyjes <72809149@continental.edu.pe>
2022-07-20 16:45:42 -0500

    main page - estilos

--- static/js/app.js
+++ static/js/app.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
     // Compare two passwords and validate
     $('#password2').keyup(function() {
         if ($('#password1').val() != $('#password2').val()) {
-            $('#msg-passwords').html("Password do not match").css('background-color', 'red');
+            $('#msg-passwords').html("Password do not match").css('color', 'red');
         } else {
-            $('#msg-passwords').html("Password matched").css('background-color', 'green');
+            $('#msg-passwords').html("Password matched").css('color', 'green');
         }
     });
 });
